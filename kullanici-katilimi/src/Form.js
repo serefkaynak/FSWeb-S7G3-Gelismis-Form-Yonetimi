797395bb45ed8ad19da17966df0056cea908445f
Şeref Kaynak <serefkynk@gmail.com>
2023-11-06 15:10:47 +0300

    developed more

--- kullanici-katilimi/src/Form.js
+++ kullanici-katilimi/src/Form.js
@@ -13,15 +13,19 @@ export default function Form () {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formData);
-    axios
-      .post("https://reqres.in/api/users", formData)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        console.log(`Form Datası Çekildi:`,formData);
+
+        axios
+        .post("https://reqres.in/api/users", formData)
+        .then((res) => {console.log(`Form Datası API'a gönderildi.:`,res.data);})
+        .catch((err) => {console.log(err);});
+
+        setFormData({
+            name: '',
+            email: '',
+            password: '',
+            kosul: false
+        });
     }
     
     const handleFormDataChange = (event) => {
@@ -29,47 +33,74 @@ export default function Form () {
     }
 
     return (
+      <div>
         <div>
-            <div>
-                <form onSubmit={handleSubmit} >
-                    <p>
-                    <label>
-                    Name:
-                    <input type="text"  placeholder='name giriniz' name="name" onChange={handleFormDataChange}/>
-                    </label>
-                    </p>
+          <form onSubmit={handleSubmit}>
+            <p>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  placeholder="name giriniz"
+                  name="name"
+                  onChange={handleFormDataChange}
+                  value={formData.name}
+                />
+              </label>
+            </p>
 
-                    <p>
-                    <label>
-                    Email:
-                    <input type="email" placeholder='email giriniz' name="email" onChange={handleFormDataChange}
-                    />
-                    </label>
-                    </p>
+            <p>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  placeholder="email giriniz"
+                  name="email"
+                  onChange={handleFormDataChange}
+                  value={formData.email}
+                />
+              </label>
+            </p>
 
-                    <p>
-                    <label>
-                    Password
-                    <input type="password" placeholder='*****' name="password" onChange={handleFormDataChange}/>
-                    </label>
-                    </p>
+            <p>
+              <label>
+                Password
+                <input
+                  type="password"
+                  placeholder="*****"
+                  name="password"
+                  onChange={handleFormDataChange}
+                  value={formData.password}
+                />
+              </label>
+            </p>
 
-                    <p>
-                    <label>
-                    Okudum, Kabul Ediyorum.
-                    <input type="checkbox" />
-                    </label>
-                    </p>
+            <p>
+              <label>
+                Okudum, Kabul Ediyorum.
+                <input 
+                    type="checkbox"
+                    name="kosul"
+                    onChange={handleFormDataChange}
+                    checked={formData.kosul}
+                    value={formData.kosul}
+                />
+              </label>
+            </p>
 
-                    <p>
-                    <button type='submit' onSubmit={handleSubmit}>
-                        Submit
-                    </button>
-                    </p>
-                    
-                </form> 
-            </div>
-            
+            <p>
+              <button 
+                type="submit" 
+                onSubmit={handleSubmit}
+                value="Submit"
+                disabled={formData.kosul ? false : true}
+                style={{backgroundColor: formData.kosul ? "green" : "red"}}
+              >
+                Submit
+              </button>
+            </p>
+          </form>
         </div>
-    )
+      </div>
+    );
 }
