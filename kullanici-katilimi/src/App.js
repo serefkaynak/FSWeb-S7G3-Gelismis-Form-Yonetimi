f362e0851816a9adfcd41228e3d11e2fdfd94942
Şeref Kaynak <serefkynk@gmail.com>
2023-11-08 17:59:25 +0300

    All questions answered

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Form from './Form';
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 
 
 function App() {
+  const [kullanicilar, setKullanicilar] = useState([]);
+  
+  const yeniKullaniciEkle = (yeniKullanici) => {
+    setKullanicilar([...kullanicilar, yeniKullanici]);
+  };
+
   return (
     <div className="App">
-      <Form/>
+      {kullanicilar.map((kullanici) => (
+        <div className='kullanici-container'>
+          <p><strong>İsim:</strong>{kullanici.name}</p>
+          <p><strong>Eposta:</strong>{kullanici.email}</p>
+          <p>
+            <strong>Kullanım Koşulları:</strong>
+            {kullanici.kosul
+              ? "Kullanıcı kosulları Onay"
+              : "Kullanıcı kosulları Onay"}
+          </p>
+        </div>
+      ))}
+      <Form yeniKullaniciEkle={yeniKullaniciEkle} />
     </div>
   );
 }
--- kullanici-katilimi/src/Form.js
+++ kullanici-katilimi/src/Form.js
@@ -1,87 +1,95 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-import * as Yup from 'yup';
+    import React, { useEffect } from "react";
+    import { useState } from "react";
+    import axios from "axios";
+    import * as Yup from "yup";
 
-
-export default function Form () {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
+    export default function Form({yeniKullaniciEkle}) {
+      const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
         kosul: false,
-    });
-    const [formErrors, setFormErrors] = useState({
-        name: '',
-        email: '',
-        password: '',
-        kosul: '',
-    });
+      });
+      const [formErrors, setFormErrors] = useState({
+        name: "",
+        email: "",
+        password: "",
+        kosul: "",
+      });
 
-    const [formValid, setFormValid] = useState(false);
+      const [formValid, setFormValid] = useState(false);
 
-  const formSchema = Yup.object().shape({
-      name: Yup.string()
-          .required('İsim Soyisim alanı gereklidir.'),
-      email: Yup.string()
-          .email('Geçerli bir email adresi giriniz.')
-          .required('Email alanı gereklidir.'),
-      password: Yup.string()
-          .required('Şifre alanı gereklidir.')
-          .min(8, 'Şifre en az 8 karakter olmalıdır.'),
-      kosul: Yup.boolean()
-          .oneOf([true], 'Kullanım Koşullarını Kabul Etmelisiniz.'),
-  });
+      const formSchema = Yup.object().shape({
+        name: Yup.string().required("İsim Soyisim alanı gereklidir."),
+        email: Yup.string()
+          .email("Geçerli bir email adresi giriniz.")
+          .required("Email alanı gereklidir."),
+        password: Yup.string()
+          .required("Şifre alanı gereklidir.")
+          .min(8, "Şifre en az 8 karakter olmalıdır."),
+        kosul: Yup.boolean().oneOf(
+          [true],
+          "Kullanım Koşullarını Kabul Etmelisiniz."
+        ),
+      });
 
-    const [kullanicilar, setKullanicilar] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    const submitHandler = (e) => {
+      const submitHandler = (e) => {
         e.preventDefault();
 
         if (formValid) {
           // dataları axios ile servea gönder
+          axios
+            .post("https://reqres.in/api/users", formData)
+            .then((res) => {
+              console.log("Server dan cevap geldi.", res.data);
+              //yeni kullanıcıyı app componentine gönder
+              yeniKullaniciEkle(res.data);
+            })
+            .catch((err) => {
+              console.log("Server dan hata geldi.", err);
+            });
         } else {
           // kullanıcıyı bilgilendir.
-          console.warn('Form Datası valid değil');
+          console.warn("Form Datası valid değil");
         }
-        console.log("formData > ",formData);
-    };
+        console.log("formData > ", formData);
+      };
 
-    
-    const inputChangeHandler = (e) => {
-      const { name, value, type, checked } = e.target;
+      const inputChangeHandler = (e) => {
+        const { name, value, type, checked } = e.target;
 
-      setFormData({
-        ...formData,
-        [name]: type === "checkbox" ? checked : value,
-      });
-
-      Yup.reach(formSchema, name)
-        .validate(type === "checkbox" ? checked : value)
-        .then((valid) => {
-          setFormErrors({
-            ...formErrors,[name]: ""});
-        })
-        .catch((err) => {
-          setFormErrors({
-            ...formErrors,[name]: err.errors[0]});
+        setFormData({
+          ...formData,
+          [name]: type === "checkbox" ? checked : value,
         });
-    };
-    
-    useEffect(() => {
-        formSchema.isValid(formData)
-        .then((valid) => setFormValid(valid));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [formData]);
 
-    useEffect(() => {
-      console.error("formError :", formErrors)
-    }, [formErrors]);
-    
-    return (
+        Yup.reach(formSchema, name)
+          .validate(type === "checkbox" ? checked : value)
+          .then((valid) => {
+            setFormErrors({
+              ...formErrors,
+              [name]: "",
+            });
+          })
+          .catch((err) => {
+            setFormErrors({
+              ...formErrors,
+              [name]: err.errors[0],
+            });
+          });
+      };
+
+      useEffect(() => {
+        formSchema.isValid(formData).then((valid) => setFormValid(valid));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [formData]);
+
+      useEffect(() => {
+        console.error("formError :", formErrors);
+      }, [formErrors]);
+
+      return (
         <div>
-          {errorMessage && <p>{errorMessage}</p>}
           <form onSubmit={submitHandler}>
             <h2>Kullanıcı Formu</h2>
             <hr></hr>
@@ -97,9 +105,11 @@ export default function Form () {
                   value={formData.name}
                 />
               </label>
-              <p>{formErrors.name}</p>
+              {formErrors.name && (
+                <div className="input-error">{formErrors.name}</div>
+              )}
             </div>
-  
+
             <div>
               <label>
                 Email:
@@ -112,9 +122,12 @@ export default function Form () {
                   value={formData.email}
                 />
               </label>
+              {formErrors.email && (
+                <div className="input-error">{formErrors.email}</div>
+              )}
             </div>
-  
-            <div >
+
+            <div>
               <label>
                 Şifre:
                 <input
@@ -126,8 +139,11 @@ export default function Form () {
                   value={formData.password}
                 />
               </label>
+              {formErrors.password && (
+                <div className="input-error">{formErrors.password}</div>
+              )}
             </div>
-  
+
             <div>
               <label>
                 Kullanım Şartları:
@@ -139,21 +155,16 @@ export default function Form () {
                   checked={formData.kosul}
                 />
               </label>
+              {formErrors.kosul && (
+                <div className="input-error">{formErrors.kosul}</div>
+              )}
             </div>
-            <div >
-              <button type="submit" disabled={!formValid} >
+            <div>
+              <button type="submit" disabled={!formValid}>
                 Gönder
               </button>
             </div>
           </form>
-          <h2>Kullanıcılar</h2>
-          <div>
-            {kullanicilar.map((user) => (
-              <p key={user.id}>
-                {user.name} {user.email} {user.password} {user.rol} {String(user.kosul)}
-              </p>
-            ))}
-          </div>
         </div>
       );
-  }
+    }
